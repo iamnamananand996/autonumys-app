d4d916100986db6e106051fdc0854f1feda04101
iamnamananand996 <iamnamananand996@gmail.com>
2025-01-24 00:01:14 +0530

    fix: fix

--- components/WalletButton/index.tsx
+++ components/WalletButton/index.tsx
@@ -1,30 +1,30 @@
-'use client'
+"use client";
 
-import { WalletIcon } from '@heroicons/react/24/outline'
-import React, { useCallback, useState } from 'react'
-import useMediaQuery from '@/hooks/useMediaQuery'
-import { PreferredExtensionModal } from '../PreferredExtensionModal'
+import React, { useCallback, useState } from "react";
+import { PreferredExtensionModal } from "../PreferredExtensionModal";
 
 export const WalletButton: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const isDesktop = useMediaQuery('(min-width: 1024px)')
+  const [isOpen, setIsOpen] = useState(false);
 
-  const onClick = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault()
-    setIsOpen(true)
-  }, [])
-  const onClose = useCallback(() => setIsOpen(false), [])
+  const onClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      e.preventDefault();
+      setIsOpen(true);
+    },
+    []
+  );
+  const onClose = useCallback(() => setIsOpen(false), []);
 
   return (
     <>
       <button
         onClick={onClick}
-        className={`h-10 ${isDesktop ? 'w-36' : 'w-10 p-2'
+        className={`h-10 w-36 p-2'
           } from-buttonLightFrom to-buttonLightTo dark:from-buttonDarkFrom dark:to-buttonDarkTo dark:bg-boxDark rounded-full bg-gradient-to-r font-medium text-white md:mt-3`}
       >
-        {isDesktop ? 'Connect Wallet' : <WalletIcon className='size-6' />}
+        Connect Wallet
       </button>
       <PreferredExtensionModal isOpen={isOpen} onClose={onClose} />
     </>
-  )
-}
+  );
+};
--- providers/WalletProvider.tsx
+++ providers/WalletProvider.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   activate,
@@ -6,222 +6,253 @@ import {
   createConnection,
   DomainRuntime,
   networks,
-} from '@autonomys/auto-utils'
-import { sendGAEvent } from '@next/third-parties/google'
-import { InjectedExtension } from '@polkadot/extension-inject/types'
-import { getWalletBySource } from '@subwallet/wallet-connect/dotsama/wallets'
-import { WalletType } from 'constants/wallet'
-import { useSafeLocalStorage } from 'hooks/useSafeLocalStorage'
-import { getSession, signOut } from 'next-auth/react'
-import { useParams } from 'next/navigation'
-import { createContext, FC, ReactNode, useCallback, useEffect, useState } from 'react'
-import type { ChainParam } from 'types/app'
-import type { WalletAccountWithType } from 'types/wallet'
-import { formatAddress } from 'utils/formatAddress'
+} from "@autonomys/auto-utils";
+import { sendGAEvent } from "@next/third-parties/google";
+import { InjectedExtension } from "@polkadot/extension-inject/types";
+import { getWalletBySource } from "@subwallet/wallet-connect/dotsama/wallets";
+import { WalletType } from "constants/wallet";
+import { useSafeLocalStorage } from "hooks/useSafeLocalStorage";
+import { useParams } from "next/navigation";
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import type { ChainParam } from "types/app";
+import type { WalletAccountWithType } from "types/wallet";
+import { formatAddress } from "utils/formatAddress";
 
-export type DomainsApis = { [key: string]: ApiPromise }
+export type DomainsApis = { [key: string]: ApiPromise };
 
 export type WalletContextValue = {
-  api: ApiPromise | undefined
-  domainsApis: DomainsApis
-  isReady: boolean
-  accounts: WalletAccountWithType[] | undefined | null
-  error: Error | null
-  injector: InjectedExtension | null
-  actingAccount: WalletAccountWithType | undefined
-  extensions: InjectedExtension[] | undefined
-  disconnectWallet: () => void
-  setActingAccount: (account: WalletAccountWithType) => void
-  setPreferredExtension: (extension: string) => void
-  preferredExtension: string | undefined
-  subspaceAccount: string | undefined
-  handleSelectFirstWalletFromExtension: (source: string) => Promise<void>
-  changeAccount: (account: WalletAccountWithType) => void
-}
+  api: ApiPromise | undefined;
+  domainsApis: DomainsApis;
+  isReady: boolean;
+  accounts: WalletAccountWithType[] | undefined | null;
+  error: Error | null;
+  injector: InjectedExtension | null;
+  actingAccount: WalletAccountWithType | undefined;
+  extensions: InjectedExtension[] | undefined;
+  disconnectWallet: () => void;
+  setActingAccount: (account: WalletAccountWithType) => void;
+  setPreferredExtension: (extension: string) => void;
+  preferredExtension: string | undefined;
+  subspaceAccount: string | undefined;
+  handleSelectFirstWalletFromExtension: (source: string) => Promise<void>;
+  changeAccount: (account: WalletAccountWithType) => void;
+};
 
 export const WalletContext = createContext<WalletContextValue>(
   // @ts-expect-error It's a good practice not to give a default value even though the linter tells you so
-  {},
-)
+  {}
+);
 
 type Props = {
-  children?: ReactNode
-}
+  children?: ReactNode;
+};
 
 export const WalletProvider: FC<Props> = ({ children }) => {
-  const { chain } = useParams<ChainParam>()
-  const [api, setApi] = useState<ApiPromise>()
-  const [domainsApis, setDomainsApis] = useState<DomainsApis>({})
-  const [isReady, setIsReady] = useState(false)
-  const [accounts, setAccounts] = useState<WalletAccountWithType[] | null | undefined>(undefined)
-  const [extensions] = useState<InjectedExtension[] | undefined>(undefined)
-  const [error, setError] = useState<Error | null>(null)
-  const [injector, setInjector] = useState<InjectedExtension | null>(null)
-  const [actingAccount, setActingAccount] = useState<WalletAccountWithType | undefined>(undefined)
-  const [subspaceAccount, setSubspaceAccount] = useState<string | undefined>(undefined)
-  const [preferredAccount, setPreferredAccount] = useSafeLocalStorage('localAccount', null)
-  const [preferredExtension, setPreferredExtension] = useSafeLocalStorage('extensionSource', null)
+  const { chain } = useParams<ChainParam>();
+  const [api, setApi] = useState<ApiPromise>();
+  const [domainsApis, setDomainsApis] = useState<DomainsApis>({});
+  const [isReady, setIsReady] = useState(false);
+  const [accounts, setAccounts] = useState<
+    WalletAccountWithType[] | null | undefined
+  >(undefined);
+  const [extensions] = useState<InjectedExtension[] | undefined>(undefined);
+  const [error, setError] = useState<Error | null>(null);
+  const [injector, setInjector] = useState<InjectedExtension | null>(null);
+  const [actingAccount, setActingAccount] = useState<
+    WalletAccountWithType | undefined
+  >(undefined);
+  const [subspaceAccount, setSubspaceAccount] = useState<string | undefined>(
+    undefined
+  );
+  const [preferredAccount, setPreferredAccount] = useSafeLocalStorage(
+    "localAccount",
+    null
+  );
+  const [preferredExtension, setPreferredExtension] = useSafeLocalStorage(
+    "extensionSource",
+    null
+  );
 
   const prepareApi = useCallback(async () => {
     try {
-      return await activate({ networkId: chain })
+      return await activate({ networkId: chain });
     } catch (error) {
-      console.error('Failed to prepare API for chain', chain, 'error:', error)
+      console.error("Failed to prepare API for chain", chain, "error:", error);
     }
-  }, [chain])
+  }, [chain]);
 
   const prepareDomainsApis = useCallback(async () => {
     try {
-      const network = networks.find((network) => network.id === chain)
-      if (!network) return
+      const network = networks.find((network) => network.id === chain);
+      if (!network) return;
 
-      const autoEvmRpc = network.domains.find((domain) => domain.runtime === DomainRuntime.AUTO_EVM)
-        ?.rpcUrls[0]
-      const autoIdRpc = network.domains.find((domain) => domain.runtime === DomainRuntime.AUTO_ID)
-        ?.rpcUrls[0]
-      if (!autoEvmRpc || !autoIdRpc) return
+      const autoEvmRpc = network.domains.find(
+        (domain) => domain.runtime === DomainRuntime.AUTO_EVM
+      )?.rpcUrls[0];
+      const autoIdRpc = network.domains.find(
+        (domain) => domain.runtime === DomainRuntime.AUTO_ID
+      )?.rpcUrls[0];
+      if (!autoEvmRpc || !autoIdRpc) return;
 
       const domainsRpcs = network.domains.map((domain) =>
-        domain.rpcUrls[0].replace('https://', 'wss://'),
-      )
+        domain.rpcUrls[0].replace("https://", "wss://")
+      );
 
       const [autoEvm, autoId] = await Promise.all(
-        domainsRpcs.flatMap((rpc) => createConnection(rpc)),
-      )
+        domainsRpcs.flatMap((rpc) => createConnection(rpc))
+      );
       return {
         autoEvm,
         autoId,
-      }
+      };
     } catch (error) {
-      console.error('Failed to prepare domains API for chain', chain, 'error:', error)
+      console.error(
+        "Failed to prepare domains API for chain",
+        chain,
+        "error:",
+        error
+      );
     }
-  }, [chain])
+  }, [chain]);
 
   const setup = useCallback(async () => {
-    setApi(await prepareApi())
-    const domainsApis = await prepareDomainsApis()
-    if (domainsApis) setDomainsApis(domainsApis)
-  }, [prepareApi, prepareDomainsApis])
-
-  const signOutSessionOnAccountChange = useCallback(async (subspaceAccount?: string) => {
-    const session = await getSession()
-    if (!subspaceAccount || (session && session?.user?.subspace?.account !== subspaceAccount))
-      await signOut({ redirect: false })
-  }, [])
+    setApi(await prepareApi());
+    const domainsApis = await prepareDomainsApis();
+    if (domainsApis) setDomainsApis(domainsApis);
+  }, [prepareApi, prepareDomainsApis]);
 
   const changeAccount = useCallback(
     async (account: WalletAccountWithType) => {
       try {
         const type =
-          account.type === WalletType.subspace || (account as { type: string }).type === 'sr25519'
+          account.type === WalletType.subspace ||
+          (account as { type: string }).type === "sr25519"
             ? WalletType.subspace
-            : WalletType.ethereum
+            : WalletType.ethereum;
         setActingAccount({
           ...account,
           type,
-        })
-        const _subspaceAccount = formatAddress(account.address)
-        setSubspaceAccount(_subspaceAccount)
-        setPreferredAccount(account.address)
-        const newInjector = extensions?.find((extension) => extension.name === account.source)
-        if (newInjector) setInjector(newInjector)
-        setIsReady(true)
-        await signOutSessionOnAccountChange(_subspaceAccount)
+        });
+        const _subspaceAccount = formatAddress(account.address);
+        setSubspaceAccount(_subspaceAccount);
+        setPreferredAccount(account.address);
+        const newInjector = extensions?.find(
+          (extension) => extension.name === account.source
+        );
+        if (newInjector) setInjector(newInjector);
+        setIsReady(true);
         sendGAEvent({
-          event: 'wallet_select_account',
+          event: "wallet_select_account",
           value: `source:${account.source}`,
-        })
+        });
       } catch (error) {
-        console.error('Failed to change account', error)
+        console.error("Failed to change account", error);
       }
     },
-    [extensions, setPreferredAccount, signOutSessionOnAccountChange],
-  )
+    [extensions, setPreferredAccount]
+  );
 
   const disconnectWallet = useCallback(async () => {
-    setInjector(null)
-    setAccounts([])
-    setActingAccount(undefined)
-    setSubspaceAccount(undefined)
-    setPreferredAccount(null)
-    setPreferredExtension(null)
-    setIsReady(false)
-    await signOutSessionOnAccountChange()
-    sendGAEvent('event', 'wallet_disconnect')
-  }, [setPreferredAccount, setPreferredExtension, signOutSessionOnAccountChange])
+    setInjector(null);
+    setAccounts([]);
+    setActingAccount(undefined);
+    setSubspaceAccount(undefined);
+    setPreferredAccount(null);
+    setPreferredExtension(null);
+    setIsReady(false);
+    sendGAEvent("event", "wallet_disconnect");
+  }, [setPreferredAccount, setPreferredExtension]);
 
   const handleGetWalletFromExtension = useCallback(
     async (source: string) => {
-      const wallet = getWalletBySource(source)
+      const wallet = getWalletBySource(source);
       if (wallet) {
-        await wallet.enable()
-        if (wallet.extension) setInjector(wallet.extension)
-        const walletAccounts = (await wallet.getAccounts()) as WalletAccountWithType[]
-        setAccounts(walletAccounts)
-        setPreferredExtension(source)
+        await wallet.enable();
+        // @ts-ignore:next-line
+        if (wallet.extension) setInjector(wallet.extension);
+        const walletAccounts =
+          (await wallet.getAccounts()) as WalletAccountWithType[];
+        setAccounts(walletAccounts);
+        setPreferredExtension(source);
         sendGAEvent({
-          event: 'wallet_get_wallet',
+          event: "wallet_get_wallet",
           value: `source:${source}`,
-        })
-        return walletAccounts
+        });
+        return walletAccounts;
       }
     },
-    [setPreferredExtension],
-  )
+    [setPreferredExtension]
+  );
 
   const handleSelectFirstWalletFromExtension = useCallback(
     async (source: string) => {
-      const walletAccounts = await handleGetWalletFromExtension(source)
-      if (!walletAccounts || walletAccounts.length === 0) return
-      const mainAccount = walletAccounts.find((account) => account.source === source)
-      if (mainAccount) changeAccount(mainAccount)
+      const walletAccounts = await handleGetWalletFromExtension(source);
+      if (!walletAccounts || walletAccounts.length === 0) return;
+      const mainAccount = walletAccounts.find(
+        (account) => account.source === source
+      );
+      if (mainAccount) changeAccount(mainAccount);
     },
-    [handleGetWalletFromExtension, changeAccount],
-  )
+    [handleGetWalletFromExtension, changeAccount]
+  );
 
   const handleConnectToExtensionAndAccount = useCallback(
     async (address: string, source: string) => {
-      const walletAccounts = await handleGetWalletFromExtension(source)
-      if (!walletAccounts || walletAccounts.length === 0) return
-      const mainAccount = walletAccounts.find((account) => account.address === address)
-      if (mainAccount) changeAccount(mainAccount)
+      const walletAccounts = await handleGetWalletFromExtension(source);
+      if (!walletAccounts || walletAccounts.length === 0) return;
+      const mainAccount = walletAccounts.find(
+        (account) => account.address === address
+      );
+      if (mainAccount) changeAccount(mainAccount);
       sendGAEvent({
-        event: 'wallet_auto_connect_account',
+        event: "wallet_auto_connect_account",
         value: `source:${source}`,
-      })
+      });
     },
-    [handleGetWalletFromExtension, changeAccount],
-  )
+    [handleGetWalletFromExtension, changeAccount]
+  );
 
   useEffect(() => {
     // This effect is used to get the injector from the selected account
     // and is triggered when the accounts or the actingAccountIdx change
     const getInjector = async () => {
-      const { web3FromSource } = await import('@polkadot/extension-dapp')
+      const { web3FromSource } = await import("@polkadot/extension-dapp");
 
       if (actingAccount?.source) {
         try {
-          const injector = await web3FromSource(actingAccount?.source)
+          const injector = await web3FromSource(actingAccount?.source);
 
-          setInjector(injector)
+          setInjector(injector);
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (e: any) {
-          setError(e)
+          setError(e);
         }
       }
-    }
+    };
 
-    getInjector()
-  }, [actingAccount])
+    getInjector();
+  }, [actingAccount]);
 
   useEffect(() => {
-    setup()
-  }, [injector, setup])
+    setup();
+  }, [injector, setup]);
 
   useEffect(() => {
     if (!actingAccount && preferredExtension && preferredAccount)
-      handleConnectToExtensionAndAccount(preferredAccount, preferredExtension)
-  }, [actingAccount, preferredAccount, preferredExtension, handleConnectToExtensionAndAccount])
+      handleConnectToExtensionAndAccount(preferredAccount, preferredExtension);
+  }, [
+    actingAccount,
+    preferredAccount,
+    preferredExtension,
+    handleConnectToExtensionAndAccount,
+  ]);
 
   // This effect is used to mock the wallet when the environment variables are set in the .env file
   useEffect(() => {
@@ -234,13 +265,15 @@ export const WalletProvider: FC<Props> = ({ children }) => {
         address: process.env.REACT_APP_MOCK_WALLET_ADDRESS,
         source: process.env.REACT_APP_MOCK_WALLET_SOURCE,
         type: WalletType.subspace,
-      }
-      setActingAccount(mockAccount)
-      setAccounts([mockAccount])
-      setSubspaceAccount(formatAddress(process.env.REACT_APP_MOCK_WALLET_ADDRESS))
-      setIsReady(true)
+      };
+      setActingAccount(mockAccount);
+      setAccounts([mockAccount]);
+      setSubspaceAccount(
+        formatAddress(process.env.REACT_APP_MOCK_WALLET_ADDRESS)
+      );
+      setIsReady(true);
     }
-  }, [])
+  }, []);
 
   return (
     <WalletContext.Provider
@@ -264,5 +297,5 @@ export const WalletProvider: FC<Props> = ({ children }) => {
     >
       {children}
     </WalletContext.Provider>
-  )
-}
+  );
+};
